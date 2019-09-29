bba2a1f29feb0b32ae01fa266d60f48d52b93337
Duy <etn218n@gmail.com>
2019-09-29 13:17:42 +0300

    Update Circle class

--- Circle.js
+++ Circle.js
@@ -1,125 +1,19 @@
-class Circle {
-    constructor(x, y, r = 0.4) {
-        this.transform = new Matrix2D();
-        this.transform.Translate(new Vector2D(x, y));
-
-        this.XAxis = new Vector2D(1, 0);
-        this.YAxis = new Vector2D(0, 1);
+class Circle extends Particle{
+    constructor(r = 0.4, x = 0, y = 0) {
+        super(x, y);
 
         this.radius = r;
-
-        this.mass = 1;
-        this.hasGravity = true;
-
-        this.velocity = new Vector2D();
-        this.angularVelocity = 0;
-
-        this.interTransform = new Matrix2D();
-        this.isInterpolated = true;
-
-        this.Renderer  = this.Draw.bind(this);
-        this.Simulator = this.Simulate.bind(this);
-
-        Engine.OnRender.push(this.Renderer);
-        Engine.OnFixedUpdate.push(this.Simulator);
-    }
-
-    Disable() {
-        Engine.OnRender.remove(this.Renderer);
-        Engine.OnFixedUpdate.remove(this.Simulator);
-    }
- 
-    Draw() {
-        if (this.isInterpolated) {
-            let interporlate = Engine.DeltaTime / Engine.FixedDeltaTime;
-        
-            this.interTransform.n02 += (this.transform.n02 - this.interTransform.n02) * interporlate;
-            this.interTransform.n12 += (this.transform.n12 - this.interTransform.n12) * interporlate;
-
-            let deltaAngle = 0;
-
-            if (this.transform.angle < this.interTransform.angle)
-                deltaAngle = (this.transform.angle + 360 - this.interTransform.angle) * interporlate;
-            else
-                deltaAngle = (this.transform.angle - this.interTransform.angle) * interporlate;
-
-            let rotationDir = this.angularVelocity < 0 ? -1 : 1;
-
-            this.interTransform.Rotate(deltaAngle * rotationDir);
-
-            this.DrawBody(this.interTransform);
-            this.DrawAxes(this.interTransform);
-        }
-        else {
-            this.DrawBody(this.transform);
-            this.DrawAxes(this.transform);
-        } 
+        this.axisLength = r + 1;
     }
 
     DrawBody(transform) {
         Context.save();
         Context.beginPath();
         Context.lineWidth = 0.1;
-        Context.strokeStyle = 'gray';
+        Context.strokeStyle = this.color;
         Context.arc(transform.n02, transform.n12, this.radius, 0, 2 * Math.PI, false);
         Context.stroke();
         Context.closePath();
         Context.restore();
-    }
-
-    DrawAxes(transform) {
-        let XAxis = transform.Multiply(this.XAxis),
-            YAxis = transform.Multiply(this.YAxis);
-
-        // x axis
-        Context.save();
-        Context.beginPath();
-        Context.lineWidth = 0.1;
-        Context.strokeStyle = "#ff6767"; // very light red
-        Context.moveTo(transform.n02, transform.n12);
-        Context.lineTo(XAxis.x, XAxis.y);
-        Context.stroke();
-        Context.closePath();
-
-        // y axis
-        Context.beginPath();
-        Context.lineWidth = 0.1;
-        Context.strokeStyle = "#009b4e"; // dark cyan
-        Context.moveTo(transform.n02, transform.n12);
-        Context.lineTo(YAxis.x, YAxis.y);
-        Context.stroke();
-        Context.closePath();
-        Context.restore();
-    }
-
-    Translate(displacement) { this.transform.Translate(displacement);}
-    LocalTranslate(displacement) { this.transform.LocalTranslate(displacement);}
-
-    Up() { return this.transform.Up(); }
-    Right() { return this.transform.Right(); }
-    Position() { return this.transform.Position(); }
-    PositionX() { return this.transform.PositionX(); }
-    PositionY() { return this.transform.PositionY(); }
-    Rotate(degree) { this.transform.Rotate(degree); }
-
-    Simulate() {
-        let scaledVelocity = Vector.Scale(this.velocity, Engine.SecondsPerFixedUpdate);
-
-        // save the last postion updated by FixUpdate
-        this.interTransform.n02 = this.transform.n02;
-        this.interTransform.n12 = this.transform.n12;
-
-        if (this.hasGravity) {
-            this.velocity.y += -9.81 * Engine.SecondsPerFixedUpdate;
-            this.transform.Translate(scaledVelocity);
-        }
-        else {  
-            this.transform.Translate(scaledVelocity);
-        }
-
-        if (this.angularVelocity != 0) {
-            this.interTransform.angle = this.transform.angle;
-            this.transform.Rotate(this.angularVelocity * Engine.SecondsPerFixedUpdate);
-        }
     }
 }
--- Week 39 & 40/Exercise1.js
+++ Week 39 & 40/Exercise1.js
@@ -1,22 +1,20 @@
-let cirA, cirB;
+function Exercise1() {
+    let cirA = new Circle(0.4, 3, 3);
+        cirA.Translate(-2, 3);
+        cirA.hasGravity = false;
+        cirA.velocity = new Vector2D(1, 0);
 
-Engine.OnAwake.push(() => {
-    cirA = new Circle(3, 3, 0.4);
-    cirA.Translate(new Vector2D(-2, 3));
-    cirA.hasGravity = false;
-    cirA.velocity = new Vector2D(1, 0);
+    let cirB = new Circle(1);
+        cirB.hasGravity = false;
+        cirB.velocity = new Vector2D(1, 1);
 
-    cirB = new Circle(0, 0, 1);
-    cirB.hasGravity = false;
-    cirB.velocity = new Vector2D(1, 1);
-});
+    Engine.OnFixedUpdate.push(() => {
+        let sumRadiusSquare = Math.pow(cirA.radius + cirB.radius, 2);
+            centerDistanceSquare = Math.pow(cirA.PositionX() - cirB.PositionX(), 2) + Math.pow(cirA.PositionY() - cirB.PositionY(), 2)
 
-Engine.OnFixedUpdate.push(() => {
-    let sumRadiusSquare = Math.pow(cirA.radius + cirB.radius, 2);
-        centerDistanceSquare = Math.pow(cirA.PositionX() - cirB.PositionX(), 2) + Math.pow(cirA.PositionY() - cirB.PositionY(), 2)
-
-    if (centerDistanceSquare <= sumRadiusSquare) {
-        cirA.Disable();
-        cirB.Disable();
-    }
-});
+        if (centerDistanceSquare <= sumRadiusSquare) {
+            cirA.Disable();
+            cirB.Disable();
+        }
+    });
+}
