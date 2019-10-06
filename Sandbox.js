399e7031458177f3ef79625b8dead6ae1b0660a3
Duy <etn218n@gmail.com>
2019-10-06 20:48:09 +0300

    Collision Demo

--- Sandbox.js
+++ Sandbox.js
@@ -1,16 +1,10 @@
 Engine.OnAwake.push(() => {
-    Exercise3();
+    //Exercise3();
 
-    // for (let i = 0; i < 20; i++) {
-    //     let p = new Rectangle(2, 2, Random(-30, 30), Random(-30, 30));
-    //         p.velocity = new Vector2D(Random(-6, 6), Random(-6, 6));
-    // }
-
-    // let rects = [];
-
-    // for (let i = 0; i < 20; i++) {
-    //     let rect = new Rectangle(2, 2, 40, -28 + i * 3);
-    //         rect.velocity = new Vector2D(Random(-30, -20), 0);
-    //     rects.push(rect);
-    // }
+    for (let i = 0; i < 100; i++) {
+        let squareLength = Random(1, 2);
+        let rect = new Rectangle(squareLength, squareLength);
+            rect.Translate(Random(-35, 35), Random(-25, 25));
+            rect.velocity = new Vector2D(Random(-10, 10), Random(-10, 10));
+    }
 });
